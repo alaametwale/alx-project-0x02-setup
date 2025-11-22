5e2e034b0a73b7d9142fa0e1e473278997c56e30
alaametwale <alaameto86@gmail.com>
2025-11-22 15:35:55 +0200

    Fix project structure to match ALX requirements

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full p-4 bg-gray-800 text-white text-center">
+      <h1 className="text-xl font-semibold">Welcome to ALX Project 0x02</h1>
+    </header>
+  );
+}
