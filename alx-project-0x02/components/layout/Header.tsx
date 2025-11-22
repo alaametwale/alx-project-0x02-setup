7cef326e3040fbfd90283bc8b6d1a63662605683
alaametwale <alaameto86@gmail.com>
2025-11-22 16:47:32 +0200

    Add PostCard and UserCard components with pages/posts and pages/users

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <nav className="bg-gray-100 p-4 mb-4 flex gap-4">
-      <Link href="/home" className="text-blue-500 hover:underline">Home</Link>
-      <Link href="/about" className="text-blue-500 hover:underline">About</Link>
-      <Link href="/posts" className="text-blue-500 hover:underline">Posts</Link>
-    </nav>
+    <header className="bg-gray-100 p-4 flex gap-4">
+      <Link href="/home" className="hover:underline">Home</Link>
+      <Link href="/about" className="hover:underline">About</Link>
+      <Link href="/posts" className="hover:underline">Posts</Link>
+      <Link href="/users" className="hover:underline">Users</Link>
+    </header>
   );
 }
